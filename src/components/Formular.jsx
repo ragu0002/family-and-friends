1f182977c6cd06b489b5978441b233a57883a4c8
Ragnhild <ragu0002@stud.kea.dk>
2025-11-21 11:41:33 +0100

    feat-add-additional-finctionality

--- src/components/Formular.jsx
+++ src/components/Formular.jsx
@@ -1,55 +1,9 @@
-"use client";
-import { useActionState } from "react";
-import { submitAnimal } from "@/app/action/action";
+import ProductForm from "./ProductForm";
 
 const Formular = () => {
-  const [state, postAnimal] = useActionState(submitAnimal, { error: {} });
-  console.log(state);
   return (
     <div>
-      <form className="mt-10 grid gap-5" action={postAnimal}>
-        <h1 className="text-center text-2xl">Add your own pet</h1>
-        {state.succes && (
-          <p className="capitalize">{`${state.productName} submitted`}</p>
-        )}
-        {state.error?.productName && (
-          <p className="text-red-500">{state.error.productName}</p>
-        )}
-        <input
-          type="text"
-          className="rounded-xl border border-black p-3 text-center capitalize"
-          name="productname"
-          defaultValue={state.productName}
-          placeholder="Animal Name"
-        />
-
-        {state.error?.productNumber && (
-          <p className="text-red-500">{state.error.productNumber}</p>
-        )}
-        <input
-          className="rounded-xl border border-black p-3 text-center"
-          name="productnumber"
-          defaultValue={state.productNumber}
-          placeholder="Animal Age"
-        />
-
-        {state.error?.productDescription && (
-          <p className="text-red-500">{state.error.productDescription}</p>
-        )}
-        <input
-          type="text"
-          className="rounded-xl border border-black p-3 text-center"
-          name="productdescription"
-          defaultValue={state.productDescription}
-          placeholder="Add a short description of your animal"
-        />
-        <button
-          type="submit"
-          className="bg-button-pink rounded-xl border border-none border-black px-4 py-2 text-white"
-        >
-          Add Animal
-        </button>
-      </form>
+      <ProductForm />
     </div>
   );
 };
